1e33a9c862921f5e6193c1042c6d8563205d4cd6
Yuri Palacio <yurifpalacio@gmail.com>
2021-04-08 22:47:06 -0300

    refactor: componentizando a aplicação
    Conforme o que foi proposto no desafio, realizado a
    componentização da aplicação da SideBar e Content
    demovendo a estrutura do arquivo App.

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,45 @@
-export function SideBar() {
+import { useEffect, useState } from 'react';
+
+import { Button } from './Button';
+
+import { api } from '../services/api';
+
+interface SideBarProps {
+  selectedGenreId: number;
+  handleClickButton: Function;
+}
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export function SideBar(props: SideBarProps) {
   // Complete aqui
+  const { selectedGenreId, handleClickButton } = props;
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
